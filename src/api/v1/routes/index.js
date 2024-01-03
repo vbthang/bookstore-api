870bf55e808f45d37119bb61ebaef294a460090b
thangvb <bathangvu@gmail.com>
2024-01-03 22:32:37 +0700

    UPDATE: Add middleware check APIKEY & Permission

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -1,14 +1,13 @@
 'use strict'
 
 const express = require('express')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
+// Check apiKey
+router.use(apiKey)
+// Check permission
+router.use(permission('0000'))
 
 router.use('/v1/api', require('./access'))
-
-// router.get('/', (req, res, next) => {
-//   return res.status(200).json({
-//     message: 'Hello World!!!'
-//   })
-// })
 
 module.exports = router
